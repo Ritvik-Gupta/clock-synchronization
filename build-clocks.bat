f9e0edc48c2481ef8be9a39e7c52e247506837cb
Ritvik Gupta <ritvik.gupta2019@vitstudent.ac.in>
2022-09-10 18:30:04 +0530

    Customized a build script for Master and Slave System Architectures

--- build-clocks.bat
+++ build-clocks.bat
@@ -1,7 +1,37 @@
 @echo off
 
-cargo build --release --no-default-features --features master,persistence
-copy .\target\release\native_demo.exe .\binary\master.exe
+echo:
+if "%1" == "master" goto correct_system_arch
+if "%1" == "slave"  goto correct_system_arch
 
-cargo build --release --no-default-features --features slave,dhat-profile
-copy .\target\release\native_demo.exe .\binary\slave.exe
+echo Invalid System Architecture "%1"
+goto program_end
+
+:correct_system_arch
+
+set SYSTEM_ARCH=%1
+shift
+
+echo Building for System Architecture "%SYSTEM_ARCH%"
+echo:
+
+set BUILD_SCRIPT=cargo build --release --no-default-features --features %SYSTEM_ARCH%
+
+:do_while
+    if (%1) == () goto end_while
+    
+    set BUILD_SCRIPT=%BUILD_SCRIPT%,%1
+    shift
+    
+    goto do_while
+:end_while
+
+echo Running command:
+echo:
+echo "%BUILD_SCRIPT%"
+echo:
+
+%BUILD_SCRIPT%
+copy .\target\release\native_demo.exe .\binary\%SYSTEM_ARCH%.exe
+
+:program_end
